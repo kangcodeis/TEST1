1b4ce41bad2c39d8817cfc17f2aeeed6bebd507c
kangcodeis <miiren543@damu.net>
2022-03-29 10:20:40 +0900

    넌작스

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const app = express()
+
+app.set('view engine','html')
+nunjucks.configure('views',{
+    express:app,
+})
+
+app.get('/',(req,res)=>{
+    res.render('index') 
+})
+
+app.listen (3000,()=>{
+    console.log('server 3000')
+})
